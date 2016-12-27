48ddeb6273b816931236f6b6436dd50a096f7ed5
Eugine Kang <kangeugine@gmail.com>
2016-12-27 15:47:55 -0800

    practice files

---
+++ basic_charts/bar_chart.js
@@ -0,0 +1,30 @@
+function draw(data) {
+      
+      /*
+        D3.js setup code
+      */
+
+          "use strict";
+          var margin = 75,
+              width = 1400 - margin,
+              height = 600 - margin;
+          
+          debugger;
+          
+          var svg = d3.select("body")
+            .append("svg")
+              .attr("width", width + margin)
+              .attr("height", height + margin)
+            .append('g')
+                .attr('class','chart');
+
+      /*
+        Dimple.js Chart construction code
+      */
+
+          var myChart = new dimple.chart(svg, data);
+          var x = myChart.addTimeAxis("x", "year"); 
+          myChart.addMeasureAxis("y", "attendance");
+          myChart.addSeries(null, dimple.plot.bar);
+          myChart.draw();
+        };
